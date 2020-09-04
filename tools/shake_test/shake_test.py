547a6989bc1e70bb9565e1b29a67a5cd35d2bd40
liqinxing <liqinxing@kedacom.com>
2020-09-04 20:16:26 +0800

    ADD: 优化过长的函数 ADD: 增加最大横向和纵向位移的判断标准 ADD: 增加图像变形程度的标准

--- tools/shake_test/shake_test.py
+++ tools/shake_test/shake_test.py
@@ -1,9 +1,11 @@
 import cv2
-from PySide2.QtWidgets import QMainWindow, QFileDialog
+from PySide2.QtWidgets import QMainWindow, QFileDialog, QMessageBox
 from PySide2.QtGui import QImage, QPixmap
 from PySide2.QtCore import QTimer
 from ui.windows.shake_test_window import Ui_ShakeTestWindow
 import numpy as np
+import math
+
 
 class Direction():
     """
@@ -15,6 +17,7 @@ class Direction():
     up = 2
     down = 4
     source = 8
+
 
 class ShakeTestTool(object):
     def __init__(self):
@@ -45,7 +48,36 @@ class ShakeTestTool(object):
             None, '打开文件', './', 'video files(*.mp4)')
         self.ui.videopath.setText(videopath[0])
 
+    def find_center_point_index(self, x, y, winSize=90000):
+        """
+        寻找到中心最近的特征点
+        """
+        center_index = -1
+        for i, point in enumerate(self.p0):
+            now_distance = (point[0][0]-x)*(point[0][0]-x) + \
+                (point[0][1]-y)*(point[0][1]-y)
+            if(now_distance < winSize):
+                center_index = i
+                winSize = now_distance
+        return center_index
+    
+    def calc_distance_anypoint(self,input_array):
+        """
+        计算每个特征点到中心的距离
+        """
+        distance_anypoint = list()
+        for i, point in enumerate(input_array):
+            if(i != self.center_index):
+                now_point = point.ravel()
+                center_point = input_array[self.center_index].ravel()
+                distance_anypoint.append(math.sqrt((now_point[0]-center_point[0])*(now_point[0]-center_point[0])
+                                                            + (now_point[1]-center_point[1])*(now_point[1]-center_point[1])))
+        return distance_anypoint
+
     def process_video(self):
+        # 输出参数初始化
+        self.dewarp_sum = 0
+        self.dewarp_count = 0
         self.vidcap = cv2.VideoCapture(self.ui.videopath.text())
         success, frame = self.vidcap.read()
         if success:
@@ -53,53 +85,118 @@ class ShakeTestTool(object):
             # 返回所有检测特征点，需要输入图片，角点的最大数量，品质因子，minDistance=7如果这个角点里有比这个强的就不要这个弱的
             self.p0 = cv2.goodFeaturesToTrack(
                 self.old_gray, mask=None, **self.feature_params)
+
+            # 获取角点的精确坐标
+            self.p0 = cv2.cornerSubPix(self.old_gray, self.p0, (5, 5), (-1, -1),
+                                       criteria=(cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 30, 0.001))
             # 创建一个mask, 用于进行横线的绘制
             self.mask = np.zeros_like(self.old_gray)
-            # 随机颜色条
-            # self.color = np.random.randint(0, 255, (50, 3))
+
+            # 寻找到中心最近的特征点
+            center_x = frame.shape[1]
+            center_y = frame.shape[0]
+            self.center_index = self.find_center_point_index(
+                center_x, center_y)
+
+            # 如果在距离中心直径300px的范围内没有找到，那么退出
+            if(self.center_index == -1):
+                reply = QMessageBox.critical(
+                    self.window, '警告', '图像中心没有找到特征点，请重新拍摄视频',
+                    QMessageBox.Yes, QMessageBox.Yes)
+                return
+
+            # 初始化横纵方向上的坐标
+            self.min_y_coord = self.max_y_coord = self.p0[self.center_index].ravel()[1]
+            self.min_x_coord = self.max_x_coord = self.p0[self.center_index].ravel()[0]
+
+            # 计算每个特征点到中心的距离
+            self.old_distance_anypoint = self.calc_distance_anypoint(self.p0)
+
+            # 增加定时器，每100ms进行一帧的处理
             self.video_timer.start(100)
             self.video_timer.timeout.connect(self.open_frame)
+        else:
+            reply = QMessageBox.critical(
+                    self.window, '警告', '视频打不开',
+                    QMessageBox.Yes, QMessageBox.Yes)
+            return
+
+    def display(self, img):
+        image = QImage(
+            img.data, img.shape[1], img.shape[0], QImage.Format_Indexed8)
+        temp_pixmap = QPixmap.fromImage(image)
+        self.ui.videoview.setPixmap(temp_pixmap)
+        self.ui.videoview.setScaledContents(True)
+
+    def draw_track(self,good_new,new_gray):
+        for i, (new, old) in enumerate(zip(good_new, self.p0)):
+            a, b = new.ravel()
+            c, d = old.ravel()
+
+            self.mask = cv2.line(
+                self.mask, (a, b), (c, d), 255, 2)
+            frame = cv2.circle(new_gray, (a, b), 5, 255, -1)
+
+        # 将两个图片进行结合，并进行图片展示
+        img = cv2.add(frame, self.mask)
+
+        # 更新前一帧图片和角点的位置
+        self.old_gray = new_gray.copy()
+        self.p0 = good_new.reshape(-1, 1, 2)
+
+        # display
+        self.display(img)
 
     def open_frame(self):
+        self.dewarp_count += 1
         success, frame = self.vidcap.read()
         if success:
             new_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            
+
             # 进行光流检测需要输入前一帧和当前图像及前一帧检测到的角点
             pl, st, err = cv2.calcOpticalFlowPyrLK(
                 self.old_gray, new_gray, self.p0, None, **self.lk_params)
             # 在原角点的基础上寻找亚像素角点，其中，criteria是设置寻找亚像素角点的参数，
-			# 采用的停止准则是最大循环次数30和最大误差容限0.001
+            # 采用的停止准则是最大循环次数30和最大误差容限0.001
             pl = cv2.cornerSubPix(new_gray, pl, (5, 5), (-1, -1),
-								criteria = (cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 30, 0.001))
+                                  criteria=(cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 30, 0.001))
 
-            # 读取运动了的角点st == 1表示检测到的运动物体，即v和u表示为0
-            good_new = pl[st == 1]
-            good_old = self.p0[st == 1]
+            now_distance_anypoint = self.calc_distance_anypoint(pl)
 
             # 绘制轨迹
-            for i, (new, old) in enumerate(zip(good_new, good_old)):
-                a, b = new.ravel()
-                c, d = old.ravel()
-                
-                self.mask = cv2.line(
-                    self.mask, (a, b), (c, d), 255, 2)
-                frame = cv2.circle(new_gray, (a, b), 5, 255, -1)
+            self.draw_track(pl,new_gray)
 
-            # 将两个图片进行结合，并进行图片展示
-            img = cv2.add(frame, self.mask)
+            # 计算每个点与中心点的距离，将之与第一帧对比，得出变形程度
+            distance_diff_sum = 0
+            for (old, new) in zip(self.old_distance_anypoint, now_distance_anypoint):
+                distance_diff = (old-new)/old
+                distance_diff_sum += distance_diff
+            print(distance_diff_sum/len(self.old_distance_anypoint))
 
-            # 更新前一帧图片和角点的位置
-            self.old_gray = new_gray.copy()
-
-            self.p0 = good_new.reshape(-1, 1, 2)
-
-            # display
-            image = QImage(
-                img.data, img.shape[1], img.shape[0], QImage.Format_Indexed8)
-            temp_pixmap = QPixmap.fromImage(image)
-            self.ui.videoview.setPixmap(temp_pixmap)
-            self.ui.videoview.setScaledContents(True)
+            if(self.dewarp_sum < distance_diff_sum/len(self.old_distance_anypoint)):
+                self.dewarp_sum = distance_diff_sum/len(self.old_distance_anypoint)
+                self.ui.warp_ratio.setValue(self.dewarp_sum)
+            
+            self.calc_center_distance(pl)
+            self.ui.center_max_y_distance.setValue(self.max_y_coord-self.min_y_coord)
+            self.ui.center_max_x_distance.setValue(self.max_x_coord-self.min_x_coord)
+            # self.dewarp_sum += distance_diff_sum / \
+            #     len(self.old_distance_anypoint)
+            # dewarp_ratio = self.dewarp_sum/self.dewarp_count
+            # print(dewarp_ratio)
+            # self.ui.warp_ratio.setValue(dewarp_ratio)
+          
 
     def cancel_process_video(self):
         self.video_timer.stop()
+    
+    def calc_center_distance(self,input_array):
+        center_point = input_array[self.center_index].ravel()
+        if(self.min_y_coord > center_point[1]):
+            self.min_y_coord = center_point[1]
+        elif(self.max_y_coord < center_point[1]):
+            self.max_y_coord = center_point[1]
+        if(self.min_x_coord > center_point[0]):
+            self.min_x_coord = center_point[0]
+        elif(self.max_x_coord < center_point[0]):
+            self.max_x_coord = center_point[0]
--- ui/windows/shake_test_window.py
+++ ui/windows/shake_test_window.py
@@ -36,26 +36,6 @@ class Ui_ShakeTestWindow(object):
         self.groupBox_2.setMaximumSize(QSize(270, 16777215))
         self.gridLayout_3 = QGridLayout(self.groupBox_2)
         self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.horizontalLayout_3 = QHBoxLayout()
-        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.radioButton_2 = QRadioButton(self.groupBox_2)
-        self.direction = QButtonGroup(ShakeTestWindow)
-        self.direction.setObjectName(u"direction")
-        self.direction.addButton(self.radioButton_2)
-        self.radioButton_2.setObjectName(u"radioButton_2")
-        self.radioButton_2.setChecked(True)
-
-        self.horizontalLayout_3.addWidget(self.radioButton_2)
-
-        self.radioButton = QRadioButton(self.groupBox_2)
-        self.direction.addButton(self.radioButton)
-        self.radioButton.setObjectName(u"radioButton")
-
-        self.horizontalLayout_3.addWidget(self.radioButton)
-
-
-        self.gridLayout_3.addLayout(self.horizontalLayout_3, 0, 0, 1, 1)
-
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.label = QLabel(self.groupBox_2)
@@ -77,7 +57,7 @@ class Ui_ShakeTestWindow(object):
         self.horizontalLayout.addWidget(self.openvideo)
 
 
-        self.gridLayout_3.addLayout(self.horizontalLayout, 1, 0, 1, 1)
+        self.gridLayout_3.addLayout(self.horizontalLayout, 0, 0, 1, 1)
 
 
         self.verticalLayout.addWidget(self.groupBox_2)
@@ -91,26 +71,22 @@ class Ui_ShakeTestWindow(object):
         self.groupBox_3.setMaximumSize(QSize(270, 16777215))
         self.gridLayout_4 = QGridLayout(self.groupBox_3)
         self.gridLayout_4.setObjectName(u"gridLayout_4")
-        self.verticalSpacer_3 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.label_3 = QLabel(self.groupBox_3)
+        self.label_3.setObjectName(u"label_3")
 
-        self.gridLayout_4.addItem(self.verticalSpacer_3, 2, 0, 1, 1)
+        self.horizontalLayout_5.addWidget(self.label_3)
 
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.isok = QPushButton(self.groupBox_3)
-        self.isok.setObjectName(u"isok")
-        self.isok.setMaximumSize(QSize(121, 16777215))
+        self.warp_ratio = QDoubleSpinBox(self.groupBox_3)
+        self.warp_ratio.setObjectName(u"warp_ratio")
+        self.warp_ratio.setButtonSymbols(QAbstractSpinBox.NoButtons)
+        self.warp_ratio.setDecimals(4)
 
-        self.horizontalLayout_2.addWidget(self.isok)
-
-        self.cancel_button = QPushButton(self.groupBox_3)
-        self.cancel_button.setObjectName(u"cancel_button")
-        self.cancel_button.setMaximumSize(QSize(121, 16777215))
-
-        self.horizontalLayout_2.addWidget(self.cancel_button)
+        self.horizontalLayout_5.addWidget(self.warp_ratio)
 
 
-        self.gridLayout_4.addLayout(self.horizontalLayout_2, 3, 0, 1, 1)
+        self.gridLayout_4.addLayout(self.horizontalLayout_5, 3, 0, 1, 1)
 
         self.horizontalLayout_4 = QHBoxLayout()
         self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
@@ -119,35 +95,56 @@ class Ui_ShakeTestWindow(object):
 
         self.horizontalLayout_4.addWidget(self.label_2)
 
-        self.doubleSpinBox = QDoubleSpinBox(self.groupBox_3)
-        self.doubleSpinBox.setObjectName(u"doubleSpinBox")
-        self.doubleSpinBox.setButtonSymbols(QAbstractSpinBox.NoButtons)
-        self.doubleSpinBox.setDecimals(4)
+        self.center_max_x_distance = QDoubleSpinBox(self.groupBox_3)
+        self.center_max_x_distance.setObjectName(u"center_max_x_distance")
+        self.center_max_x_distance.setButtonSymbols(QAbstractSpinBox.NoButtons)
+        self.center_max_x_distance.setDecimals(4)
 
-        self.horizontalLayout_4.addWidget(self.doubleSpinBox)
+        self.horizontalLayout_4.addWidget(self.center_max_x_distance)
 
 
         self.gridLayout_4.addLayout(self.horizontalLayout_4, 0, 0, 1, 1)
 
-        self.horizontalLayout_5 = QHBoxLayout()
-        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
-        self.label_3 = QLabel(self.groupBox_3)
-        self.label_3.setObjectName(u"label_3")
+        self.horizontalLayout_6 = QHBoxLayout()
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.label_4 = QLabel(self.groupBox_3)
+        self.label_4.setObjectName(u"label_4")
 
-        self.horizontalLayout_5.addWidget(self.label_3)
+        self.horizontalLayout_6.addWidget(self.label_4)
 
-        self.doubleSpinBox_2 = QDoubleSpinBox(self.groupBox_3)
-        self.doubleSpinBox_2.setObjectName(u"doubleSpinBox_2")
-        self.doubleSpinBox_2.setButtonSymbols(QAbstractSpinBox.NoButtons)
-        self.doubleSpinBox_2.setDecimals(4)
+        self.center_max_y_distance = QDoubleSpinBox(self.groupBox_3)
+        self.center_max_y_distance.setObjectName(u"center_max_y_distance")
+        self.center_max_y_distance.setButtonSymbols(QAbstractSpinBox.NoButtons)
+        self.center_max_y_distance.setDecimals(4)
 
-        self.horizontalLayout_5.addWidget(self.doubleSpinBox_2)
+        self.horizontalLayout_6.addWidget(self.center_max_y_distance)
 
 
-        self.gridLayout_4.addLayout(self.horizontalLayout_5, 1, 0, 1, 1)
+        self.gridLayout_4.addLayout(self.horizontalLayout_6, 1, 0, 1, 1)
 
 
         self.verticalLayout.addWidget(self.groupBox_3)
+
+        self.verticalSpacer_3 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer_3)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.isok = QPushButton(self.centralwidget)
+        self.isok.setObjectName(u"isok")
+        self.isok.setMaximumSize(QSize(121, 16777215))
+
+        self.horizontalLayout_2.addWidget(self.isok)
+
+        self.cancel_button = QPushButton(self.centralwidget)
+        self.cancel_button.setObjectName(u"cancel_button")
+        self.cancel_button.setMaximumSize(QSize(121, 16777215))
+
+        self.horizontalLayout_2.addWidget(self.cancel_button)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
 
 
         self.gridLayout.addLayout(self.verticalLayout, 0, 1, 1, 1)
@@ -180,17 +177,16 @@ class Ui_ShakeTestWindow(object):
     # setupUi
 
     def retranslateUi(self, ShakeTestWindow):
-        ShakeTestWindow.setWindowTitle(QCoreApplication.translate("ShakeTestWindow", u"MainWindow", None))
+        ShakeTestWindow.setWindowTitle(QCoreApplication.translate("ShakeTestWindow", u"\u9632\u6296\u6d4b\u8bd5\u5de5\u5177", None))
         self.groupBox_2.setTitle(QCoreApplication.translate("ShakeTestWindow", u"\u8bbe\u7f6e", None))
-        self.radioButton_2.setText(QCoreApplication.translate("ShakeTestWindow", u"\u6c34\u5e73", None))
-        self.radioButton.setText(QCoreApplication.translate("ShakeTestWindow", u"\u5782\u76f4", None))
         self.label.setText(QCoreApplication.translate("ShakeTestWindow", u"\u89c6\u9891\u6587\u4ef6", None))
         self.openvideo.setText(QCoreApplication.translate("ShakeTestWindow", u"\u6253\u5f00\u6587\u4ef6", None))
         self.groupBox_3.setTitle(QCoreApplication.translate("ShakeTestWindow", u"\u7ed3\u679c", None))
+        self.label_3.setText(QCoreApplication.translate("ShakeTestWindow", u"\u56fe\u7247\u53d8\u5f62\u7a0b\u5ea6", None))
+        self.label_2.setText(QCoreApplication.translate("ShakeTestWindow", u"\u4e2d\u5fc3\u70b9\u6700\u5927\u6a2a\u5411\u4f4d\u79fb", None))
+        self.label_4.setText(QCoreApplication.translate("ShakeTestWindow", u"\u4e2d\u5fc3\u70b9\u6700\u5927\u7eb5\u5411\u4f4d\u79fb", None))
         self.isok.setText(QCoreApplication.translate("ShakeTestWindow", u"\u5f00\u59cb", None))
         self.cancel_button.setText(QCoreApplication.translate("ShakeTestWindow", u"\u505c\u6b62", None))
-        self.label_2.setText(QCoreApplication.translate("ShakeTestWindow", u"\u4e2d\u5fc3\u70b9\u4f4d\u79fb", None))
-        self.label_3.setText(QCoreApplication.translate("ShakeTestWindow", u"\u56fe\u7247\u53d8\u5f62\u7a0b\u5ea6", None))
         self.groupBox.setTitle(QCoreApplication.translate("ShakeTestWindow", u"\u89c6\u9891", None))
         self.videoview.setText("")
     # retranslateUi
