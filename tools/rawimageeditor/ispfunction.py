00c7b769f8bf5dcba27fe325d6cda1ce4b05789c
xiaoqinxing <1415289814@qq.com>
2021-02-01 18:03:10 +0800

    RAW编辑工具 DEBUG 解决编译不通过的问题

---
+++ tools/rawimageeditor/ispfunction.py
@@ -0,0 +1,25 @@
+import tools.rawimageeditor.isp as isp
+import tools.rawimageeditor.debayer as debayer
+
+pipeline_dict = {
+    "raw":          isp.get_src_raw_data,
+    "original raw": isp.get_src_raw_data,
+    "black level":  isp.black_level_correction,
+    "BLC":          isp.black_level_correction,
+    "rolloff":      None,
+    "ABF":          None,
+    "demosaic":     debayer.demosaic,
+    "awb":          isp.channel_gain_white_balance,
+    "AWB":          isp.channel_gain_white_balance,
+    "ccm":          isp.color_correction,
+    "CCM":          None,
+    "gamma":        isp.gamma_correction,
+    "LTM":          isp.ltm_correction,
+    "advanced chroma enhancement":  None,
+    "ACE":                          None,
+    "wavelet denoise":              None,
+    "WNR":                          None,
+    "adaptive spatial filter":      None,
+    "ASF":                          None,
+    "bad pixel correction":         isp.bad_pixel_correction
+}
