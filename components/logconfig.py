e8d4e87791d327a5d52690c004e39a24033bd5a0
xiaoqinxing <1415289814@qq.com>
2021-09-27 22:59:53 +0800

    日志 FIX 修复logging中文格式问题

--- components/logconfig.py
+++ components/logconfig.py
@@ -1,36 +1,36 @@
+import datetime
+import os
 import logging
 
-import datetime   # 时间模块
-import os
-
 # 设置日志存放路径
-path = '.\\log\\'
-if(not os.path.exists(path)):
-    os.mkdir(path)
+LOGFILE_PATH = '.\\log\\'
+if not os.path.exists(LOGFILE_PATH):
+    os.mkdir(LOGFILE_PATH)
 
 # 获取今天的日期 格式2019-08-01
-today_date = str(datetime.date.today())
+TODAY_DATE = str(datetime.date.today())
 
 
 def init_log():
-    logging.basicConfig(
-        filename=path + today_date + '.log',
-        level=logging.DEBUG, filemode='a',
-        format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s',
-        datefmt='%Y-%m-%d %H:%M',
-        encoding='utf-8'
-    )
+    """
+    func: 初始化日志，只需要在主函数中初始化一次
+    """
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(
+        LOGFILE_PATH + TODAY_DATE + '.log', 'a', 'utf-8')
+    formatter = logging.Formatter(
+        '%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+    handler.setFormatter(formatter)
+    root_logger.addHandler(handler)
     logging.info('logging module init is ok')
 
 
 def clean_old_log():
     """
-    删除今天以外的日志
+    删除除今天以外的日志
     """
-    global path
-    global today_date
-
     # 遍历目录下的所有日志文件 i是文件名
-    for i in os.listdir(path):
-        if(i != today_date + '.log'):
-            os.remove(path + i)
+    for i in os.listdir(LOGFILE_PATH):
+        if i != TODAY_DATE + '.log':
+            os.remove(LOGFILE_PATH + i)
