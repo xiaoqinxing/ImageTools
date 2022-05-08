a14404fc11fd11699314839fcef727792ea0555f
xiaoqinxing <1415289814@qq.com>
2022-05-08 23:10:12 +0800

    ADD: 增加yuvviewer的UI

---
+++ tools/yuv_viewer/ui/yuvviewer_window.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'yuvviewer_window.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+import resource_rc
+
+class Ui_YUVEditor(object):
+    def setupUi(self, YUVEditor):
+        if not YUVEditor.objectName():
+            YUVEditor.setObjectName(u"YUVEditor")
+        YUVEditor.resize(800, 600)
+        icon = QIcon()
+        icon.addFile(u":/tool_icon/resource/main.png", QSize(), QIcon.Normal, QIcon.Off)
+        YUVEditor.setWindowIcon(icon)
+        YUVEditor.setToolTipDuration(-1)
+        YUVEditor.setAnimated(True)
+        YUVEditor.setDockOptions(QMainWindow.AllowTabbedDocks|QMainWindow.AnimatedDocks|QMainWindow.GroupedDragging|QMainWindow.VerticalTabs)
+        self.saveimage = QAction(YUVEditor)
+        self.saveimage.setObjectName(u"saveimage")
+        icon1 = QIcon()
+        icon1.addFile(u":/tool_icon/resource/save_icon.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.saveimage.setIcon(icon1)
+        self.openimage = QAction(YUVEditor)
+        self.openimage.setObjectName(u"openimage")
+        icon2 = QIcon()
+        icon2.addFile(u":/tool_icon/resource/open.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.openimage.setIcon(icon2)
+        self.actionstats = QAction(YUVEditor)
+        self.actionstats.setObjectName(u"actionstats")
+        icon3 = QIcon()
+        icon3.addFile(u":/tool_icon/resource/stats.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.actionstats.setIcon(icon3)
+        self.nextphoto = QAction(YUVEditor)
+        self.nextphoto.setObjectName(u"nextphoto")
+        icon4 = QIcon()
+        icon4.addFile(u":/tool_icon/resource/down.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.nextphoto.setIcon(icon4)
+        self.prephoto = QAction(YUVEditor)
+        self.prephoto.setObjectName(u"prephoto")
+        icon5 = QIcon()
+        icon5.addFile(u":/tool_icon/resource/up.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.prephoto.setIcon(icon5)
+        self.deletephoto = QAction(YUVEditor)
+        self.deletephoto.setObjectName(u"deletephoto")
+        icon6 = QIcon()
+        icon6.addFile(u":/tool_icon/resource/delete.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.deletephoto.setIcon(icon6)
+        self.add_compare = QAction(YUVEditor)
+        self.add_compare.setObjectName(u"add_compare")
+        icon7 = QIcon()
+        icon7.addFile(u":/tool_icon/resource/add.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.add_compare.setIcon(icon7)
+        self.rotateright = QAction(YUVEditor)
+        self.rotateright.setObjectName(u"rotateright")
+        icon8 = QIcon()
+        icon8.addFile(u":/tool_icon/resource/right.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.rotateright.setIcon(icon8)
+        self.centralwidget = QWidget(YUVEditor)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayout_2 = QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.photo_title = QGroupBox(self.centralwidget)
+        self.photo_title.setObjectName(u"photo_title")
+        self.gridLayout_3 = QGridLayout(self.photo_title)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.gridLayout = QGridLayout()
+        self.gridLayout.setObjectName(u"gridLayout")
+
+        self.gridLayout_3.addLayout(self.gridLayout, 0, 0, 1, 1)
+
+
+        self.gridLayout_2.addWidget(self.photo_title, 0, 0, 1, 1)
+
+        YUVEditor.setCentralWidget(self.centralwidget)
+        self.statusBar = QStatusBar(YUVEditor)
+        self.statusBar.setObjectName(u"statusBar")
+        self.statusBar.setMouseTracking(True)
+        YUVEditor.setStatusBar(self.statusBar)
+        self.toolBar = QToolBar(YUVEditor)
+        self.toolBar.setObjectName(u"toolBar")
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.toolBar.sizePolicy().hasHeightForWidth())
+        self.toolBar.setSizePolicy(sizePolicy)
+        self.toolBar.setAllowedAreas(Qt.AllToolBarAreas)
+        self.toolBar.setOrientation(Qt.Vertical)
+        YUVEditor.addToolBar(Qt.RightToolBarArea, self.toolBar)
+
+        self.toolBar.addAction(self.openimage)
+        self.toolBar.addAction(self.saveimage)
+        self.toolBar.addAction(self.deletephoto)
+        self.toolBar.addAction(self.actionstats)
+        self.toolBar.addAction(self.prephoto)
+        self.toolBar.addAction(self.nextphoto)
+        self.toolBar.addAction(self.rotateright)
+        self.toolBar.addAction(self.add_compare)
+
+        self.retranslateUi(YUVEditor)
+
+        QMetaObject.connectSlotsByName(YUVEditor)
+    # setupUi
+
+    def retranslateUi(self, YUVEditor):
+        YUVEditor.setWindowTitle(QCoreApplication.translate("YUVEditor", u"YUV\u56fe\u7247\u67e5\u770b\u5de5\u5177", None))
+#if QT_CONFIG(tooltip)
+        YUVEditor.setToolTip("")
+#endif // QT_CONFIG(tooltip)
+        self.saveimage.setText(QCoreApplication.translate("YUVEditor", u"save", None))
+#if QT_CONFIG(shortcut)
+        self.saveimage.setShortcut(QCoreApplication.translate("YUVEditor", u"Ctrl+S", None))
+#endif // QT_CONFIG(shortcut)
+        self.openimage.setText(QCoreApplication.translate("YUVEditor", u"open", None))
+#if QT_CONFIG(tooltip)
+        self.openimage.setToolTip(QCoreApplication.translate("YUVEditor", u"open", None))
+#endif // QT_CONFIG(tooltip)
+        self.actionstats.setText(QCoreApplication.translate("YUVEditor", u"\u7edf\u8ba1\u4fe1\u606f", None))
+#if QT_CONFIG(tooltip)
+        self.actionstats.setToolTip(QCoreApplication.translate("YUVEditor", u"\u7edf\u8ba1\u4fe1\u606f", None))
+#endif // QT_CONFIG(tooltip)
+        self.nextphoto.setText(QCoreApplication.translate("YUVEditor", u"\u4e0b\u4e00\u4e2a\u56fe\u7247", None))
+#if QT_CONFIG(shortcut)
+        self.nextphoto.setShortcut(QCoreApplication.translate("YUVEditor", u"Down", None))
+#endif // QT_CONFIG(shortcut)
+        self.prephoto.setText(QCoreApplication.translate("YUVEditor", u"\u4e0a\u4e00\u5f20\u56fe\u7247", None))
+#if QT_CONFIG(shortcut)
+        self.prephoto.setShortcut(QCoreApplication.translate("YUVEditor", u"Up", None))
+#endif // QT_CONFIG(shortcut)
+        self.deletephoto.setText(QCoreApplication.translate("YUVEditor", u"\u5220\u9664", None))
+#if QT_CONFIG(tooltip)
+        self.deletephoto.setToolTip(QCoreApplication.translate("YUVEditor", u"\u5220\u9664", None))
+#endif // QT_CONFIG(tooltip)
+#if QT_CONFIG(shortcut)
+        self.deletephoto.setShortcut(QCoreApplication.translate("YUVEditor", u"Del", None))
+#endif // QT_CONFIG(shortcut)
+        self.add_compare.setText(QCoreApplication.translate("YUVEditor", u"\u6dfb\u52a0\u5bf9\u6bd4\u56fe\u7247", None))
+        self.rotateright.setText(QCoreApplication.translate("YUVEditor", u"\u987a\u65f6\u9488\u65cb\u8f6c", None))
+        self.photo_title.setTitle("")
+        self.toolBar.setWindowTitle(QCoreApplication.translate("YUVEditor", u"toolBar_2", None))
+    # retranslateUi
+
--- tools/yuv_viewer/yuv_viewer.py
+++ tools/yuv_viewer/yuv_viewer.py
@@ -1,17 +1,17 @@
 from PySide2.QtWidgets import QGraphicsScene, QFileDialog, QDialog
+from isort import file
 from components.customwidget import ImageView, sceneDisplayImage, critical
 from components.window import SubWindow
 from components.histview import HistView
 from os import listdir, remove
 from os.path import isfile, join, getmtime, dirname, basename
-from .ui.imageeditor_window import Ui_ImageEditor
-from .imageeffect import ImageEffect, BlurType, WaterMarkParams
-from .ui.watermarkview import Ui_WaterMarkView
+from .ui.yuvviewer_window import Ui_YUVEditor
+from components.BasicImage import ImageBasic
 
 
 class YUVViewer(SubWindow):
     def __init__(self, name='YUVViewer', parent=None):
-        super().__init__(name, parent, Ui_ImageEditor())
+        super().__init__(name, parent, Ui_YUVEditor())
         self.scene = QGraphicsScene()
         self.imageview = ImageView(self.scene, parent)
         # 由于graphicsView被自定义了，需要重新定义一下UI，gridlayout还需要重新加一下widget
@@ -21,22 +21,15 @@ class YUVViewer(SubWindow):
         self.imageview.sigWheelEvent.connect(self.update_wheel_ratio)
         self.ui.openimage.triggered.connect(self.on_open_img)
         self.ui.saveimage.triggered.connect(self.save_now_image)
-        self.ui.compareimage.triggered.connect(self.compare_image)
-        self.ui.boxblur.triggered.connect(self.boxblur_image)
-        self.ui.guassian.triggered.connect(self.guassian_image)
-        self.ui.medianblur.triggered.connect(self.medianblur_image)
-        self.ui.bilateralblur.triggered.connect(self.bilateralblur_image)
         # self.ui.historgram.triggered.connect(self.on_calc_hist)
         self.ui.actionstats.triggered.connect(self.on_calc_stats)
         self.ui.nextphoto.triggered.connect(self.switch_next_photo)
         self.ui.prephoto.triggered.connect(self.switch_pre_photo)
         self.imageview.rubberBandChanged.connect(self.update_stats_range)
-        self.ui.watermark.triggered.connect(self.open_watermark_win)
         self.ui.deletephoto.triggered.connect(self.delete_photo)
+        self.img = ImageBasic()
         self.scale_ratio = 100
-        self.img = ImageEffect()
-        self.filepath = './'
-        self.imgfilename = ''
+        # self.img = ImageEffect()
         self.hist_window = None
 
     def displayImage(self, img):
@@ -74,7 +67,7 @@ class YUVViewer(SubWindow):
         self.__init_img(next_photo, indexstr)
 
     def switch_pre_photo(self):
-        pre_photo, index, files_nums = self.find_next_photo(self.filepath, -1)
+        pre_photo, index, files_nums = self.img.find_next_photo(-1)
         indexstr = "({}/{})".format(index + 1, files_nums)
         self.__init_img(pre_photo, indexstr)
 
@@ -84,112 +77,13 @@ class YUVViewer(SubWindow):
         self.__init_img(imagepath[0])
 
     def __init_img(self, filename, indexstr=''):
-        if (filename != ''):
-            self.img.load_image(filename)
-            self.filepath = dirname(filename)
-            self.imgfilename = basename(filename)
-            self.img.imageconvert(0)
-            if (self.img.nowImage is not None):
-                self.displayImage(self.img.nowImage)
-                self.ui.photo_title.setTitle(indexstr + self.imgfilename)
-            else:
-                critical('打开图片失败')
+        if filename != '':
+            self.img.load_imagefile(filename)
+            ret = self.img.display_in_scene(self.scene)
+            if ret is not True:
+                critical(ret)
                 return
-
-    def open_watermark_win(self):
-        if(self.img.is_load_image == True):
-            self.watermark_win = QDialog(self.imageview)
-            self.watermark_ui = Ui_WaterMarkView()
-            self.watermark_ui.setupUi(self.watermark_win)
-            self.watermark_win.show()
-            self.watermark_ui.open_watermark.clicked.connect(
-                self.open_watermark_path)
-            self.watermark_ui.change_transparent.valueChanged.connect(
-                self.set_watermark_params)
-            self.watermark_ui.change_watermark_size.valueChanged.connect(
-                self.set_watermark_params)
-            self.watermark_ui.change_watermark_th.valueChanged.connect(
-                self.set_watermark_params)
-            self.watermark_ui.change_watermark_type.currentIndexChanged.connect(
-                self.set_watermark_params)
-            self.watermark_ui.generate.clicked.connect(self.generate_watermark)
-            self.watermark_ui.analysis.clicked.connect(
-                self.analysis_space_watermark)
-        else:
-            critical('打开原图片失败，请先导入图片')
-
-    def open_watermark_path(self):
-        watermarkpath = QFileDialog.getOpenFileName(
-            None, '打开图片', './', "Images (*.jpg *.png *.bmp)")
-        self.watermark_path = watermarkpath[0]
-        if (self.watermark_path != ''):
-            self.watermark_ui.watermark_path.setText(self.watermark_path)
-            self.img.set_watermark_img(self.watermark_path)
-            self.set_watermark_params()
-        else:
-            critical('打开水印图片失败')
-
-    def get_watermark_parmas(self):
-        watermark_params = WaterMarkParams()
-        watermark_params.transparent = self.watermark_ui.change_transparent.value()
-        watermark_params.size = self.watermark_ui.change_watermark_size.value()
-        watermark_params.threshold = self.watermark_ui.change_watermark_th.value()
-        watermark_params.watermark_type = self.watermark_ui.change_watermark_type.currentIndex()
-        return watermark_params
-
-    def set_watermark_params(self):
-        self.img.set_watermark_show(self.get_watermark_parmas())
-        self.img.imageconvert(1)
-        self.displayImage(self.img.dstImage)
-
-    def generate_watermark(self):
-        self.img.imageconvert(1)
-        self.img.generate_watermark(self.get_watermark_parmas())
-        self.displayImage(self.img.dstImage)
-
-    def analysis_space_watermark(self):
-        self.img.analysis_space_watermark()
-        self.img.imageconvert(1)
-        self.displayImage(self.img.dstImage)
-
-    def boxblur_image(self):
-        if(self.img.is_load_image == True):
-            self.img.blur(BlurType.BoxBlur)
-            self.img.imageconvert(1)
-            self.displayImage(self.img.nowImage)
-
-    def guassian_image(self):
-        if(self.img.is_load_image == True):
-            self.img.blur(BlurType.GaussianBlur)
-            self.img.imageconvert(1)
-            self.displayImage(self.img.nowImage)
-
-    def medianblur_image(self):
-        if(self.img.is_load_image == True):
-            self.img.blur(BlurType.MediaBlur)
-            self.img.imageconvert(1)
-            self.displayImage(self.img.nowImage)
-
-    def bilateralblur_image(self):
-        if(self.img.is_load_image == True):
-            self.img.blur(BlurType.BilateralBlur)
-            self.img.imageconvert(1)
-            self.displayImage(self.img.nowImage)
-
-    def save_now_image(self):
-        if(self.img.is_load_image == True):
-            imagepath = QFileDialog.getSaveFileName(
-                None, '保存图片', './', "Images (*.jpg)")
-            if(imagepath[0] != ''):
-                self.img.save_image(self.img.nowImage, imagepath[0])
-
-    def compare_image(self):
-        if(self.img.is_load_image == True):
-            if(self.img.get_img_index() == 0):
-                self.img.imageconvert(1)
-            else:
-                self.img.imageconvert(0)
-            self.displayImage(self.img.nowImage)
+            self.ui.photo_title.setTitle(indexstr + self.img.imgpath)
 
     def update_stats_range(self, viewportRect, fromScenePoint, toScenePoint):
         if(toScenePoint.x() == 0 and toScenePoint.y() == 0
