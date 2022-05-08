6f7056775f6a2aa0028ab007d0c2937432b82ca7
xiaoqinxing <1415289814@qq.com>
2022-05-08 23:06:03 +0800

    ADD: 增加基础返回值组件

---
+++ components/status_code_enum.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class StatusCode(Enum):
+    """状态码枚举类"""
+    OK = '成功'
+    ERROR = '错误'
+    FILE_NOT_FOUND = '文件不存在'
+    IMAGE_FORMAT_ERR = '图像格式错误'
+    IMAGE_FORMAT_NOT_SUPPORT = '图像格式不支持'
+    IMAGE_READ_ERR = '图像读取失败'
+    IMAGE_IS_NONE = '图片为空'
